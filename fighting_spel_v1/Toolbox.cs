5eee6c36512e7007458054b8df87f9c097b4cc46
Theo zibell <theo@zibell.se>
2021-10-27 12:55:07 +0200

    last

--- fighting_spel_v1/Toolbox.cs
+++ fighting_spel_v1/Toolbox.cs
@@ -9,9 +9,13 @@ public class Toolbox
 
         while (i < 2)
         {
-            System.Console.WriteLine($"Skriv spelar {i + 1} namn");
+            System.Console.WriteLine($"Skriv spelar {i + 1} namn (Måste var mindre än 16)");
             kampe[i] = Console.ReadLine();
-            i++;
+            if (kampe[i] != "" && kampe[i].Length < 16)
+            {
+                i++;
+            }
+            else { System.Console.WriteLine("Du skriv inte något eller var det för långt"); }
             Console.Clear();
         }
 
@@ -21,13 +25,29 @@ public class Toolbox
     public static void slags(string[] kampe)
     {
         Random generator = new Random();
-        int r = generator.Next(0, 10);
         int[] hp = new int[2];
         for (int i = 0; i < 2; i++) { hp[i] = 100; }
         System.Console.WriteLine($"Spelare 1 har {hp[0]} och spelare 2 har {hp[1]}");
         Console.ReadLine();
         Console.Clear();
 
-        //while (hp)
+        while (hp[0] > 0 && hp[1] > 0)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                int r = generator.Next(0, 10);
+                hp[i] -= r;
+                System.Console.WriteLine($"{kampe[i]} tog {r} skada");
+                System.Console.WriteLine(hp[i]);
+                Console.ReadLine();
+            }
+        }
+        Console.Clear();
+        if (hp[0] < hp[1])
+        {
+            System.Console.WriteLine($"{kampe[1]} van");
+        }
+        else { System.Console.WriteLine($"{kampe[0]} van"); }
+        Console.ReadLine();
     }
 }
